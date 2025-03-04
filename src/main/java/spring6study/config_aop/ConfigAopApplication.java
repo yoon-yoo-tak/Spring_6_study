5992bab6ad98e2fd0a71c03eaeeeb0e15d790239
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2025-03-04 13:55:04 +0900

    main 브랜치 비어있도록 수정

--- src/main/java/spring6study/config_aop/ConfigAopApplication.java
+++ src/main/java/spring6study/config_aop/ConfigAopApplication.java
@@ -1,38 +1,9 @@
 package spring6study.config_aop;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-
-
 public class ConfigAopApplication {
-
     public static void main(String[] args) {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(UserService.class);
-
-        System.out.println("Check ApplicationContext");
-
-        UserService userService = ctx.getBean(UserService.class);
-        System.out.println(userService.getUser());
-    }
-
-}
-
-@Configuration
-class UserService{
-    public String getUser(){
-        return "user";
     }
 }
 
-@Component
-class MemberService{
-    public String getMember(){
-        return "member";
-    }
-}
 
 
