89faf291bd31acfc84eba9784dac4162cf47c002
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2025-03-15 00:08:02 +0900

    수정

--- src/main/java/spring6study/config_aop/parentBean/BankController.java
+++ src/main/java/spring6study/config_aop/parentBean/BankController.java
@@ -8,7 +8,7 @@ class BankController {
     // TransferServiceImpl을 직접 주입받으려고 함
     private final TransferServiceImpl transferService;
 
-    // 문제 발생: No qualifying bean of type 'TransferServiceImpl'
+    // 문제 발생: No qualifying bean ...
     public BankController(TransferServiceImpl transferService) {
         this.transferService = transferService;
     }
