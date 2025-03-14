50492fbbdc2042aa266c4ff6fe6cd7165b567534
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2025-03-15 00:11:56 +0900

    exclude 코드 추가

--- src/main/java/spring6study/config_aop/config/AppConfig.java
+++ src/main/java/spring6study/config_aop/config/AppConfig.java
@@ -2,8 +2,11 @@ package spring6study.config_aop.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Service;
 
 @Configuration
 @ComponentScan(basePackages = "spring6study.config_aop") // 패키지 내 모든 컴포넌트 자동 스캔
+                //,excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Service.class))
 public class AppConfig {
 }
