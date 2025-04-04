d6cc90d513c3ede97552609f41f7f38e87ad0fc2
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2025-04-04 23:49:31 +0900

    환경변수전달방식 코드 추가

--- src/main/java/spring6study/config_aop/config/AppConfig.java
+++ src/main/java/spring6study/config_aop/config/AppConfig.java
@@ -3,7 +3,6 @@ package spring6study.config_aop.config;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @Configuration
 public class AppConfig {
@@ -11,8 +10,12 @@ public class AppConfig {
     @Value("${server.port:8080}")
     private String port;
 
+    @Value("${myname}")
+    private String name;
+
     @PostConstruct
     public void printPort(){
         System.out.println("server port : " + port);
+        System.out.println("name = " + name);
     }
 }
