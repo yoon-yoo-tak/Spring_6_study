1dd703e510bfe043e47c1c48330282504b550c76
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2025-04-22 15:55:59 +0900

    프로젝트 추가

---
+++ homework/about_proxy/src/main/java/com/example/about_proxy/AboutProxyApplication.java
@@ -0,0 +1,13 @@
+package com.example.about_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AboutProxyApplication.class, args);
+	}
+
+}
---
+++ homework/about_proxy/src/test/java/com/example/about_proxy/AboutProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.about_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
