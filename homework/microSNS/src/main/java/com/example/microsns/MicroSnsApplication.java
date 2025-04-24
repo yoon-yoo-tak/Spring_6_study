48efc48135e2598f9e83cec9245fdf338e168f10
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2025-04-24 21:21:55 +0900

    프로젝트 세팅

---
+++ homework/microSNS/src/main/java/com/example/microsns/MicroSnsApplication.java
@@ -0,0 +1,13 @@
+package com.example.microsns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroSnsApplication.class, args);
+    }
+
+}
---
+++ homework/microSNS/src/test/java/com/example/microsns/MicroSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
